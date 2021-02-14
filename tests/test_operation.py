ea771ba2d303b25b82ea1037d76f2952221f4cf9
Ryan Miller <rmiller07@gmail.com>
2021-02-14 12:49:03 -0500

    comments

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -12,15 +12,16 @@ def test_ops(want, strategy, crv, onx, ankreth, ankr, rewards, chain, vault, wha
     voter_proxy.approveStrategy(strategy.gauge(), strategy, {"from": gov})
 
     whalebefore = want.balanceOf(whale)
+    whaledeposit = 200e18
     want.approve(vault, 2 ** 256 - 1, {"from": whale} )
-    vault.deposit(200e18, {"from": whale})
+    vault.deposit(whaledeposit, {"from": whale})
 
     strategy.harvest({'from': strategist})
     print("\n-----HARVEST1, Move LPs to strategy ----")
     genericStateOfStrat(strategy, want, vault)
     genericStateOfVault(vault, want)
 
-    chain.sleep(2592000)
+    chain.sleep(2592000) # fast forward 30 days
     chain.mine(1)
 
     strategy.harvest({'from': strategist})
@@ -28,36 +29,34 @@ def test_ops(want, strategy, crv, onx, ankreth, ankr, rewards, chain, vault, wha
 
     ankrethbal = ankreth.balanceOf(strategy)
     ethbal = strategy.balance()
-    """
-    assert ankrethbal <= 1
-    assert ethbal <= 1
-    assert crv.balanceOf(strategy) == 0
-    assert ankr.balanceOf(strategy) == 0
-    assert onx.balanceOf(strategy) == 0
-    """
+
     print("ankrETH = ", ankrethbal/1e18)
     print("ETH = ", ethbal/1e18)
 
-    print("\nVOTER BALANCES~~~~~~~~~~")
+    print("\n~VOTER BALANCES~")
     print("CRV:",crv.balanceOf(voter))
     print("ANKR:",ankr.balanceOf(voter))
     print("ONX:",onx.balanceOf(voter))
     print("ANKRETH:",ankreth.balanceOf(voter))
     print("ETH:",voter.balance())
-    print("PROXY BALANCES~~~~~~~~~~")
+    print("~PROXY BALANCES~")
     print("CRV:",crv.balanceOf(voter_proxy))
     print("ANKR:",ankr.balanceOf(voter_proxy))
     print("ONX:",onx.balanceOf(voter_proxy))
     print("ANKRETH:",ankreth.balanceOf(voter_proxy))
     print("ETH:",voter_proxy.balance())
-    print("STRAT BALANCES~~~~~~~~~~")
+    print("~STRAT BALANCES~")
     print("CRV:",crv.balanceOf(strategy))
     print("ANKR:",ankr.balanceOf(strategy))
     print("ONX:",onx.balanceOf(strategy))
     print("ANKRETH:",ankreth.balanceOf(strategy))
     print("ETH:",strategy.balance())
 
-    print("\nEstimated APR: ", "{:.2%}".format(((vault.totalAssets()-200*1e18)*12)/(200*1e18)))
+    # APR = ((Total balance after 1 month - Principle) * 12months) / Principle
+    print("\nEstimated APR: ", "{:.2%}".format(((
+        vault.totalAssets()-whaledeposit)*12)
+        /whaledeposit
+    ))
 
     vault.withdraw({"from": whale})
     print("\nWithdraw")
